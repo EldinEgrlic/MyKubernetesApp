115ecbdeac2175a69f4c144a099d90d7936980b7
Eldin Egrlic <e.egrlic@tntsoft.ba>
2021-11-11 10:38:53 +0100

    Added app

---
+++ App/MyKubernetesApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyKubernetesApp.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Home of the My Kubernetes App");
+        }
+    }
+}
